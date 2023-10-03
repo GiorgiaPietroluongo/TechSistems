01926cc2b8bcf7f2b3f7d4e3511919792094f76c
GiorgiaPietroluongo <142852498+GiorgiaPietroluongo@users.noreply.github.com>
2023-10-03 19:18:59 -0300

    first commit

---
+++ src/main/java/senac/java/Views/TelasCadastro.java
@@ -0,0 +1,22 @@
+package senac.java.Views;
+
+import senac.java.Domain.Users;
+import senac.java.Domain.Horarios;
+import senac.java.Services.CadastroCliente;
+
+import java.util.Scanner;
+
+public class InterfaceCadastros {
+
+    static Scanner sc = new Scanner(System.in);
+//    static Users users = new Users();
+    static Horarios horarios = new Horarios();
+
+    public static void cadastroCliente(){
+    }
+
+        public static void cadastroHorario(){
+
+        }
+
+}
